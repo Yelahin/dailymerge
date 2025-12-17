8d2e15d2a2a426d4a52d4308edc55cf7b6d2e58c
Illia <yelahinn@gmail.com>
2025-12-17 23:38:18 +0100

    add celery.py

---
+++ dailymerge/celery.py
@@ -0,0 +1,10 @@
+import os 
+from celery import Celery
+
+os.environ('DJANGO_SETTINGS_MODULE', 'dailymerge.settings')
+
+app = Celery('dailymerge')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
