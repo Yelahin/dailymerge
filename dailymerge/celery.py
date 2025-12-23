ebbfbe884d962645e7fc4e7bf81a52abef1b1c18
Illia <yelahinn@gmail.com>
2025-12-23 18:37:34 +0100

    set up schedule in celery.py

--- dailymerge/celery.py
+++ dailymerge/celery.py
@@ -1,9 +1,22 @@
 import os 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailymerge.settings')
 
 app = Celery('dailymerge')
+
+app.conf.beat_schedule = {
+    "upload-data": {
+        "task": "feeds.tasks.upload_data",
+        "schedule": crontab(minute="*"),
+        "args": ("https://feeds.bbci.co.uk/news/world/rss.xml",)
+    },
+    "remove-data": {
+        "task": "feeds.tasks.remove_data",
+        "shcedule": crontab(minute="*"),
+    }
+}
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
