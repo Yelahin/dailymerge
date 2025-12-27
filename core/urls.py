956f2d0cc6a56d896ab7637f63326ffc1a897397
Illia <yelahinn@gmail.com>
2025-12-27 21:01:32 +0100

    add categoreis in urls, views, basic.html

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from core.views import news_page
 
 urlpatterns = [
-    path('', news_page, name='news')
+    path('', news_page, name='news'),
+    path('<slug:slug>', news_page, name='filtered_news')
 ]
--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from feeds.models import ArticleModel, ArticleCategoryModel
 
 # Create your views here.
-def news_page(request):
-    articles = ArticleModel.objects.all()
+def news_page(request, slug="world-news"):
+    articles = ArticleModel.objects.filter(category__slug=slug)
     categories = ArticleCategoryModel.objects.all()
     return render(request, 'core/index.html', context={'articles': articles, 
                                                        "categories": categories})
