a66e7cf1b84bb12b47dc7cd5a73f5a24ab8cb2f1
Illia <yelahinn@gmail.com>
2025-12-27 13:52:15 +0100

    add feeds.feeds.py / move feeds urls in feeds.feeds.py

--- dailymerge/celery.py
+++ dailymerge/celery.py
@@ -1,6 +1,7 @@
 import os 
 from celery import Celery
 from celery.schedules import crontab
+from feeds.feeds import FEEDS_URLS
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailymerge.settings')
 
@@ -10,26 +11,7 @@ app.conf.beat_schedule = {
     "upload-data": {
         "task": "feeds.tasks.upload_data",
         "schedule": 5,
-        "args": (
-            [   
-                #World News
-                "https://feeds.bbci.co.uk/news/world/rss.xml", #world news +
-                "https://www.theguardian.com/world/rss", #world news +
-                "https://www.dailymail.co.uk/home/index.rss", #world news +
-
-                #Politics News
-                "https://rss.app/feeds/tFPubwkQz33TawYL.xml", #EU politics +
-                "https://rss.app/feeds/tM4rTuFUu0bLwKgS.xml", #US politics +
-                "https://rss.app/feeds/tGiu9CqL75md2Hcs.xml", #China politics +
-                "https://rss.app/feeds/tLIJILshtpaHgTYQ.xml", #Ukraine war politics +
-                "https://rss.app/feeds/tMUmTyRbNx0UVHKS.xml", #Russia politics +
-                "https://rss.politico.com/congress.xml", #Congress politics +
-                "https://rss.politico.com/defense.xml", #Defense politics +
-
-                #Sport News
-                "https://rss.app/feeds/twV4dtLhEH4l9cRG.xml", #ufc news
-            ], 
-        )
+        "args": (FEEDS_URLS, )
     },
     "remove-data": {
         "task": "feeds.tasks.remove_data",
---
+++ feeds/feeds.py
@@ -0,0 +1,19 @@
+FEEDS_URLS = {
+    "World News": [
+        "https://feeds.bbci.co.uk/news/world/rss.xml",
+        "https://www.theguardian.com/world/rss",
+        "https://www.dailymail.co.uk/home/index.rss",
+    ],
+    "Politics News": [
+        "https://rss.app/feeds/tFPubwkQz33TawYL.xml",
+        "https://rss.app/feeds/tM4rTuFUu0bLwKgS.xml",
+        "https://rss.app/feeds/tGiu9CqL75md2Hcs.xml",
+        "https://rss.app/feeds/tLIJILshtpaHgTYQ.xml", 
+        "https://rss.app/feeds/tMUmTyRbNx0UVHKS.xml",
+        "https://rss.politico.com/congress.xml",
+        "https://rss.politico.com/defense.xml",
+    ],
+    "Sport News": [
+        "https://rss.app/feeds/twV4dtLhEH4l9cRG.xml",
+    ]
+}
