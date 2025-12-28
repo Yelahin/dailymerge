eb146ca94040f61b9235b1d46d516f1dde69db9b
Illia <yelahinn@gmail.com>
2025-12-28 23:57:32 +0100

    add more rss feeds

--- feeds/feeds.py
+++ feeds/feeds.py
@@ -14,6 +14,18 @@ FEEDS_URLS = {
         "https://rss.politico.com/defense.xml",                     #World Defense
     ],
     "Sport News": [
-        "https://rss.app/feeds/twV4dtLhEH4l9cRG.xml",               #UFC
+        #UFC
+        "https://rss.app/feeds/twV4dtLhEH4l9cRG.xml",
+        #Boxing
+        "https://rss.app/feeds/tLR0k8xszBGDYEc8.xml",
+        #NBA
+        "https://rss.app/feeds/tLR0k8xszBGDYEc8.xml",
+        #Soccer
+        "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=fs/soccer,soccer/epl/league/1,soccer/mls/league/5,soccer/ucl/league/7,soccer/europa/league/8,soccer/wc/league/12,soccer/euro/league/13,soccer/wwc/league/14,soccer/nwsl/league/20,soccer/cwc/league/26,soccer/gold_cup/league/32,soccer/unl/league/67",
+        "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=soccer/wc/league/12",
+        #Football
+        "https://rss.app/feeds/tZfs4e5NYT7Fp9AL.xml",
+        #Olympics
+        "https://rss.app/feeds/tLvdrdNu0WAjNnxC.xml",
     ]
 }
