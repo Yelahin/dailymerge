5127ffeb8d1219c0e14ea5e8af8c26ad467bc213
Illia <yelahinn@gmail.com>
2025-12-30 22:10:21 +0100

    remove rss.app feeds / add independent feeds

--- feeds/feeds.py
+++ feeds/feeds.py
@@ -5,27 +5,19 @@ FEEDS_URLS = {
         "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",   #World news
     ],
     "Politics News": [
-        "https://rss.app/feeds/tFPubwkQz33TawYL.xml",               #EU
+        "https://feeds.bbci.co.uk/news/politics/rss.xml",           #World politics
+        "https://rss.politico.com/politics08.xml",                  #World politics
+        "https://www.theguardian.com/us-news/us-politics/rss",      #World politics
         "https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml",#US
-        "https://rss.app/feeds/tGiu9CqL75md2Hcs.xml",               #China
-        "https://rss.app/feeds/tLIJILshtpaHgTYQ.xml",               #Ukraine(war)
-        "https://rss.app/feeds/tMUmTyRbNx0UVHKS.xml",               #Russia(war)
         "https://rss.politico.com/congress.xml",                    #World Congres
         "https://rss.politico.com/defense.xml",                     #World Defense
     ],
     "Sport News": [
-        #UFC
-        "https://rss.app/feeds/twV4dtLhEH4l9cRG.xml",
-        #Boxing
-        "https://rss.app/feeds/tLR0k8xszBGDYEc8.xml",
-        #NBA
-        "https://rss.app/feeds/tLR0k8xszBGDYEc8.xml",
+        #Sport news
+        "https://api.sportskeeda.com/v3/feeds_v2/1414?limit=1000&response_type=w3c",
+        "https://deadspin.com/rss/",
         #Soccer
         "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=fs/soccer,soccer/epl/league/1,soccer/mls/league/5,soccer/ucl/league/7,soccer/europa/league/8,soccer/wc/league/12,soccer/euro/league/13,soccer/wwc/league/14,soccer/nwsl/league/20,soccer/cwc/league/26,soccer/gold_cup/league/32,soccer/unl/league/67",
         "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=soccer/wc/league/12",
-        #Football
-        "https://rss.app/feeds/tZfs4e5NYT7Fp9AL.xml",
-        #Olympics
-        "https://rss.app/feeds/tLvdrdNu0WAjNnxC.xml",
     ]
 }
