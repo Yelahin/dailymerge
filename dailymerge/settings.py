78aa88c9e1e931d9d27e542e7dabfe79e2707648
Illia <yelahinn@gmail.com>
2026-01-06 11:18:00 +0100

    add caching for categories

--- dailymerge/settings.py
+++ dailymerge/settings.py
@@ -99,6 +99,15 @@ DATABASES = {
 }
 
 
+#Cache
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
