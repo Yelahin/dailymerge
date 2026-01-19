9264cc41a676af3f156a47934f090a82d5604b5e
Illia <yelahinn@gmail.com>
2026-01-19 14:46:13 +0100

    make user login after sign up

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -12,5 +13,13 @@ def profile(request):
 
 class SignUp(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('news')
     template_name = 'registration/signup.html' 
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password1'])
+        login(self.request, user)
+        return response
+        
