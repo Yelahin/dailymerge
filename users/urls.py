ff1fea586da49abf45dba772fa40f78e102142d5
Illia <yelahinn@gmail.com>
2026-01-19 14:30:16 +0100

    add sign up

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from users.views import profile
+from users.views import profile, SignUp
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
-    path("profile/", profile, name="profile")
+    path("profile/", profile, name="profile"),
+    path("accounts/signup/", SignUp.as_view(), name="sign_up")
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 @login_required
 def profile(request):
     return render(request, 'registration/profile.html')
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html' 
