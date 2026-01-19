b80ee562a8baf56dbf5753ebbc03b8b0bd1dcf59
Illia <yelahinn@gmail.com>
2026-01-19 17:07:10 +0100

    add email field in sign up form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 
