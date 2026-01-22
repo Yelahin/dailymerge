b233b38b148996a9f683b481c0f45975aa21e482
Illia <yelahinn@gmail.com>
2026-01-22 20:24:25 +0100

    connect articles to user

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.db.models import Q
 from feeds.models import ArticleModel, ArticleCategoryModel
+from django.contrib.auth.models import AnonymousUser
 from django.core.cache import cache
 from django.shortcuts import redirect
 
@@ -14,7 +15,11 @@ class ArticleListView(ListView):
     default_category = 'world-news'
 
     def get_queryset(self):
+        user = self.request.user
+        if isinstance(user, AnonymousUser):
+            return None
         slug = self.kwargs.get('slug', self.default_category)
+        qs = ArticleModel.objects.filter(source__in=user.usersettings.source.all())
 
         #Search bar
         if 'search_bar' in self.request.GET:
@@ -22,19 +27,19 @@ class ArticleListView(ListView):
             query = Q()
             for term in search_terms:
                 query &= Q(title__icontains=term) | Q(summary__icontains=term)
-            queryset = ArticleModel.objects.filter(query)
+            queryset = qs.filter(query)
         #Categories
         else:
             #Caching for default category
             if slug == self.default_category:
                 queryset = cache.get_or_set(
-                    f'article_source_category_{slug}',
-                    ArticleModel.objects.filter(source__category__slug=slug),
+                    f'article_source_category_{self.request.user.id}',
+                    qs.filter(source__category__slug=slug),
                     30,
                 )
             #Not default category
             else:
-                queryset = ArticleModel.objects.filter(source__category__slug=slug)
+                queryset = qs.filter(source__category__slug=slug)
         return queryset.order_by("-published")
 
     def get_context_data(self, **kwargs):
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from feeds.models import Source
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -9,3 +11,8 @@ class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     source = models.ManyToManyField(Source, blank=True)
     
+
+@receiver(post_save, sender=User)
+def create_user_settings(sender, instance, created, **kwargs):
+    if created:
+        UserSettings.objects.get_or_create(user=instance)
