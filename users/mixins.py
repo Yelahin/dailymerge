e4fc626a26351a5fdc3c8e694ba9fdefeba3efcc
Illia <yelahinn@gmail.com>
2026-01-25 20:19:40 +0100

    make source logic more specific for user

--- users/mixins.py
+++ users/mixins.py
@@ -1,7 +1,11 @@
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from feeds.models import ArticleCategoryModel
+from feeds.models import ArticleCategoryModel, Source
+from typing import Iterable
+from django.contrib.auth.models import User
+from django.forms import BaseModelForm
+
 
 class AddUserSettingsMixin(LoginRequiredMixin, CreateView):
     """Set up UserSettings objects"""
@@ -69,43 +73,56 @@ class UpdateUserSettingsMixin(UpdateView):
         form = self.get_form()
 
         if form.is_valid():
-            user_setting = getattr(request.user.usersettings, self.setting_field)
-            
+            user_objects = getattr(request.user.usersettings, self.setting_field)
+
+            # Check if form have any changes
+            if not form.has_changed():
+                return HttpResponseRedirect(self.success_url)
+
             # Check if an identical object already exists
-            existing_obj = self.model.objects.filter(**form.cleaned_data).first()
             current_obj = self.object
-            if existing_obj:
-                # If it exists, delete current object and use the existing one
-                user_setting.remove(current_obj)
+            self.update_to_existing_obj(form, self.request.user, user_objects, current_obj)
+            return HttpResponseRedirect(self.success_url)
 
-                if isinstance(current_obj, ArticleCategoryModel):
-                    current_obj.sources.update(category=existing_obj)
-
-                if current_obj.user_settings.count() == 0:
-                    current_obj.delete()
-                user_setting.add(existing_obj)
-                return HttpResponseRedirect(self.success_url)
-            
-            # If this object is shared by multiple users, create a new copy
-            if current_obj.user_settings.count() > 1:
-                if self.not_editable_field:
-                    field_value = getattr(current_obj, self.not_editable_field)
-                    form.cleaned_data[self.not_editable_field] = field_value
-                
-                new_obj = self.model.objects.create(**form.cleaned_data)
-
-                if isinstance(current_obj, ArticleCategoryModel):
-                    self.request.user.usersettings.sources.filter(category=current_obj).update(category=new_obj)
-                
-                # Replace the old object with the new one for this user
-                user_setting.remove(current_obj)
-                user_setting.add(new_obj)
-                return HttpResponseRedirect(self.success_url)
-            else:
-                return self.form_valid(form)
 
         return self.form_invalid(form)
-    
+
+    @classmethod
+    def update_to_existing_obj(cls, 
+                               form: BaseModelForm,
+                               user: User, 
+                               user_objects: Iterable[Source] | Iterable[ArticleCategoryModel], 
+                               current_obj: Source | ArticleCategoryModel):
+        """Update object to existing one"""
+
+        # If object exist - use it
+        existing_obj = cls.model.objects.filter(**form.cleaned_data).first()
+        if existing_obj:
+            result_obj = existing_obj
+        # Else, check if there any not editable field and create own one 
+        else:
+            if cls.not_editable_field:
+                field_value = getattr(current_obj, cls.not_editable_field)
+                form.cleaned_data[cls.not_editable_field] = field_value
+            result_obj = cls.model.objects.create(**form.cleaned_data)
+            
+        
+        # Removing current object from UserSettings field(sources | categories)
+        user_objects.remove(current_obj)
+        # Add existing object
+        user_objects.add(result_obj)
+
+        # If objects type is ArticleCategoryModel - chage category for related sources
+        if isinstance(current_obj, ArticleCategoryModel):
+            user_sources = getattr(user.usersettings, "sources").all()
+            old_category_sources = user_sources.filter(category=current_obj)
+            old_category_sources.update(category=result_obj)
+
+
+        # Remove object from db if no one use it
+        if current_obj.user_settings.count() == 0:
+            current_obj.delete()
+
 
 class GetContextDataMixin:
     def get_context_data(self, **kwargs):
